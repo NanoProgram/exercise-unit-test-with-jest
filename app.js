7eb016a6ca5ac326546128a612bacd1e20a43e03
NanoProgram <danielj.perezd@gmail.com>
2022-12-11 17:10:33 +0000

    transfer

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log ("halo")
+// app.js file content
+const sum = (a,b) => {
+    return a + b
+}
+
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function (valueInDollar) {
+    let valueInYen = valueInDollar * 106.58;
+    return valueInYen;
+}
+
+console.log(sum(7,3))
+
+module.exports = {sum, fromEuroToDollar, fromDollarToYen}
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+
+/*const fromEuroToDollar = function(valueInEuro){
+    let valueInDollar = valueInEuro * 1.2;
+    return valueInDollar;
+}*/
